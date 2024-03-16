96d7ea403cbc2355930230255220242c26049335
Shea50 <160406484+Shea50@users.noreply.github.com>
2024-03-16 19:00:54 +0300

    Add files via upload

---
+++ control_flow.dart
@@ -0,0 +1,10 @@
+void main() {
+  var age = 30;
+  if (age > 10) {
+    print("Your number is greater than 10");
+  } else if (age < 10) {
+    print("Your number is less than 10");
+  }  else if (age == 10) {
+    print("Your number is equal to 10"); 
+  }
+}
